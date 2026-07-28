6bd478140d5c53124aa2e4c4c3a0caf7b52a10e8
竹官雨文 <2056497754@qq.com>
2026-07-28 23:59:11 +0800

    Site updated: 2026-07-28 23:59:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/21/hello-world/","2026/07/22/大模型微调技术学习/","2026/07/27/rag学习/","2026/07/27/学习计划/"];function toRandomPost(){
+var posts=["2026/07/21/hello-world/","2026/07/22/大模型微调技术学习/","2026/07/27/rag学习/","2026/07/27/学习计划/","2026/07/28/AI知识图谱GraphRAG/","2026/07/28/不了解知识汇总/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
